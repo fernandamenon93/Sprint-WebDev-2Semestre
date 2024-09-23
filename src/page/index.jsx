1ebea646c1f3fa2de585fa803373ceabcb973218
Fernanda  Rocha Menon <fernandamenon93@gmail.com>
2024-09-23 19:10:48 -0300

    add

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -3,123 +3,73 @@ import './index.css';
 
 export default function Main() {
   return (
-    <div className='main-container'>
-      <div className='background'>
-        <div className='image' />
-        <span className='projeto'>PROJETO</span>
-        <span className='impactos'>IMPACTOS</span>
-        <span className='sobre'>SOBRE</span>
-        <div className='line' />
-        <div className='image-traced' />
-        <div className='line-1' />
-        <div className='line-2' />
-        <div className='line-3' />
-      </div>
-      <span className='solution'>A SOLUÇÃO</span>
-      <div className='line-4' />
-      <div className='rectangle'>
-        <span className='interactive-site'>
-          Um site interativo dedicado aos entusiastas da Fórmula E, projetado
-          para oferecer uma experiência envolvente e informativa. Este site
-          proporciona aos usuários acesso a detalhes dos pilotos e a transmissão
-          ao vivo das corridas, aumentando assim o seu envolvimento com o
-          esporte. Uma característica distintiva e excitante deste portal é a
-          funcionalidade que permite aos usuários acumular pontos, os quais
-          podem ser utilizados colaborativamente com a comunidade para conceder
-          "poderes" aos pilotos, assemelhando-se aos itens do jogo Mario Kart.
-          Esta função inovadora eleva o nível de interação e participação dos
-          fãs, oferecendo-lhes uma oportunidade sem precedentes de influenciar o
-          desenrolar das corridas. Para obter pontos, os usuários são
-          incentivados a convidar amigos para o site e a manter uma presença
-          regular através de horas de visualização.
-        </span>
-      </div>
-      <div className='rectangle-5' />
-      <div className='rectangle-6'>
-        <span className='impacts'>IMPACTOS</span>
-        <div className='line-7' />
-        <div className='flex-row'>
-          <div className='image-8' />
-          <div className='rectangle-9'>
-            <span className='innovative-technology'>
-              Temos uma ideia inovadora e uma tecnologia revolucionária que
-              estamos confiantes de que atrairá um público ainda maior para a
-              Formula E. Estamos trabalhando em uma tecnologia que permitirá aos
-              espectadores experimentarem as corridas como nunca antes. Com essa
-              tecnologia, os fãs poderão mergulhar diretamente na ação, estamos
-              desenvolvendo uma plataforma interativa que permitirá aos
-              espectadores participarem ativamente das corridas, votando em
-              decisões estratégicas em tempo real. Estamos confiantes de que
-              essa combinação de imersão, interatividade e tecnologia de ponta
-              tornará a Formula E uma experiência obrigatória para os fãs de
-              corridas e entusiastas de tecnologia em todo o mundo.
-            </span>
+    <div>
+      <header>
+        <nav>
+          <div className="logo-container">
+            <img src="src/assets/images/logo.png" alt="Logo" />
           </div>
+          <ul>
+            <li><a href="#projeto">PROJETO</a></li>
+            <li><a href="#impactos">IMPACTOS</a></li>
+            <li><a href="#sobre">SOBRE</a></li>
+            <li><a href="#home">HOME</a></li>
+          </ul>
+        </nav>
+      </header>
+      <section class="">
+        
+      </section>
+      <section background-banner>
+      <img class="banner" src="src/assets/images/banner.png" alt="banner" />
+      </section>
+      <section id="solucao" className="background-top-main">
+        
+        <h1 className="texto-p">A SOLUÇÃO</h1>
+        <div className="container">
+          <p>
+            Um site interativo dedicado aos entusiastas da Fórmula E, projetado para oferecer uma experiência envolvente e informativa...
+          </p>
         </div>
-      </div>
-      <div className='banner' />
-      <div className='background-a'>
-        <span className='challenge'>O DESAFIO</span>
-        <div className='line-b' />
-        <span className='fiap-tech-mahindra'>
-          A FIAP juntamente com a empresa Tech Mahindra, elaborou como
-          <br />
-          Challenge de 2024 para as turmas de primeira ano de Engenharia de
-          software
-          <br />o desafio de Popularizar a Fórmula E, afim de trazer maior
-          visibilidade ao público
-          <br />
-          atráves de uma soluçao tecnológica a ser desenvolvida pelos alunos ate
-          o fim do ano.
-        </span>
-        <div className='rectangle-c' />
-        <span className='problem'>O PROBLEMA</span>
-        <div className='line-d' />
-        <div className='rectangle-e'>
-          <span className='text-b'>
-            Percepção de que os carros elétricos são menos emocionantes e
-            potentes em comparação com os carros de corrida tradicionais movidos
-            a combustível. Maior popularidade e tradição da Fórmula 1 e de
-            outras categorias de corrida mais estabelecidas, o que torna a
-            Formula E menos atrativa para alguns espectadores. Falta de
-            conhecimento sobre a categoria e suas corridas, devido à sua
-            relativa novidade em comparação com outras competições
-            automobilísticas.
-          </span>
+        <div className="red-line"></div>
+      </section>
+
+      <section className="back">
+        <h2 className="h1-p">O DESAFIO</h2>
+        <p className="container">
+        A FIAP juntamente com a empresa Tech Mahindra, elaborou como Challenge de 2024 para as turmas de primeiro ano de Engenharia de Software o desafio de Popularizar a Fórmula E, a fim de trazer maior visibilidade aopúblico através de uma solução tecnológica a ser desenvolvida pelos alunos até o fim do ano.
+        </p>
+        <div className="red-line"></div>
+      </section>
+
+      <section id="impactos" className="back-2">
+        <div className="container">
+          <h2>IMPACTOS</h2>
+          <p>
+            Temos uma ideia inovadora e uma tecnologia revolucionária que estamos confiantes de que atrairá um público ainda maior para a Fórmula E...
+          </p>
         </div>
-        <div className='image-f' />
-        <div className='line-10' />
-        <div className='image-traced-11' />
-      </div>
-      <div className='tokyo-race-start' />
-      <div className='image-removebg-preview' />
-      <div className='background-12'>
-        <span className='site-desenvolvido-equipe'>
-          Site desenvolvido pela equipe Virtual Vanguard da sala ESPV1 para
-          desenvolver e esclarecer como será <br />
-          realizado o projeto do Challenge - 2024 para as turmas de Engenharia
-          de Software da FIAP
-        </span>
-        <div className='flex-row-cf'>
-          <span className='projeto-13'>PROJETO</span>
-          <span className='impactos-14'>IMPACTOS</span>
-          <span className='sobre-15'>SOBRE</span>
-          <span className='home'>HOME</span>
-          <div className='line-16' />
-          <div className='line-17' />
-          <div className='line-18' />
-          <div className='line-19' />
+      </section>
+      <section>
+        
+      </section>
+
+      <footer className="footer">
+        
+        <div className="links">
+          <a href="#projeto">PROJETO</a>
+          <a href="#impactos">IMPACTOS</a>
+          <a href="#sobre">SOBRE</a>
+          <a href="#home">HOME</a>
         </div>
-        <div className='flex-row-1a'>
-          <div className='image-traced-1b' />
-          <div className='image-traced-1c' />
-          <div className='image-traced-1d' />
+        <div className="logos">
+          <a href="https://github.com"><img src="src/assets/images/github.png"></img></a>
+          <a href="https://linkedin.com"><img src="src/assets/images/linkedin.png"></img></a>
+          <a href="https://facebook.com"><img src="src/assets/images/facebook.png"></img></a>
         </div>
-        <span className='copyright'>
-          © Virtual Vanguard - Todos os direitos reservados - Segurança e
-          Privacidade
-        </span>
-      </div>
+        <p>© Virtual Vanguard - Todos os direitos reservados - Segurança e Privacidade</p>
+      </footer>
     </div>
   );
 }
+
